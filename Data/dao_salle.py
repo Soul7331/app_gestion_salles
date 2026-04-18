4fdf956c410f269405e034dda11ad1c7974414a1
Souleymane Kone <souleymane.on1@gmail.com>
2026-04-18 19:17:36 -0400

    Implementation des operations CRUD dans la couche DAO

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import json
+from models.salle import Salle
 
 
 class DataSalle:
@@ -14,5 +15,74 @@ class DataSalle:
             password=config["password"],
             database=config["database"]
         )
-
         return connexion
+
+    def insert_salle(self, salle):
+        connexion = self.get_connection()
+        curseur = connexion.cursor()
+
+        requete = "INSERT INTO salle (code, libelle, type, capacite) VALUES (%s, %s, %s, %s)"
+        valeurs = (salle.code, salle.libelle, salle.type, salle.capacite)
+
+        curseur.execute(requete, valeurs)
+        connexion.commit()
+
+        curseur.close()
+        connexion.close()
+
+    def update_salle(self, salle):
+        connexion = self.get_connection()
+        curseur = connexion.cursor()
+
+        requete = "UPDATE salle SET libelle=%s, type=%s, capacite=%s WHERE code=%s"
+        valeurs = (salle.libelle, salle.type, salle.capacite, salle.code)
+
+        curseur.execute(requete, valeurs)
+        connexion.commit()
+
+        curseur.close()
+        connexion.close()
+
+    def delete_salle(self, code):
+        connexion = self.get_connection()
+        curseur = connexion.cursor()
+
+        requete = "DELETE FROM salle WHERE code=%s"
+        curseur.execute(requete, (code,))
+        connexion.commit()
+
+        curseur.close()
+        connexion.close()
+
+    def get_salle(self, code):
+        connexion = self.get_connection()
+        curseur = connexion.cursor()
+
+        requete = "SELECT code, libelle, type, capacite FROM salle WHERE code=%s"
+        curseur.execute(requete, (code,))
+        resultat = curseur.fetchone()
+
+        curseur.close()
+        connexion.close()
+
+        if resultat:
+            return Salle(resultat[0], resultat[1], resultat[2], resultat[3])
+        return None
+
+    def get_salles(self):
+        connexion = self.get_connection()
+        curseur = connexion.cursor()
+
+        requete = "SELECT code, libelle, type, capacite FROM salle"
+        curseur.execute(requete)
+        resultats = curseur.fetchall()
+
+        curseur.close()
+        connexion.close()
+
+        liste_salles = []
+        for resultat in resultats:
+            salle = Salle(resultat[0], resultat[1], resultat[2], resultat[3])
+            liste_salles.append(salle)
+
+        return liste_salles
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Data.dao_salle import DataSalle
+from models.salle import Salle
 
 dao = DataSalle()
 
@@ -8,3 +9,36 @@ try:
     connexion.close()
 except Exception as e:
     print("Erreur de connexion :", e)
+
+print("\n--- TEST INSERTION ---")
+salle1 = Salle("A101", "Salle reseau", "Laboratoire", 25)
+dao.insert_salle(salle1)
+print("Salle ajoutée avec succès")
+
+print("\n--- TEST RECHERCHE ---")
+salle_trouvee = dao.get_salle("A101")
+if salle_trouvee:
+    salle_trouvee.afficher_infos()
+else:
+    print("Salle non trouvée")
+
+print("\n--- TEST MODIFICATION ---")
+salle1_modifiee = Salle("A101", "Salle informatique", "Laboratoire", 30)
+dao.update_salle(salle1_modifiee)
+print("Salle modifiée avec succès")
+
+print("\n--- TEST LISTE DES SALLES ---")
+liste = dao.get_salles()
+for salle in liste:
+    salle.afficher_infos()
+    print("----------------------")
+
+print("\n--- TEST SUPPRESSION ---")
+dao.delete_salle("A101")
+print("Salle supprimée avec succès")
+
+print("\n--- TEST FINAL LISTE DES SALLES ---")
+liste = dao.get_salles()
+for salle in liste:
+    salle.afficher_infos()
+    print("----------------------")
